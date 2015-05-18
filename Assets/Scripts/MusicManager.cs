4fdee96e674241caf8bc66ed039b0afb0d96f77e
David Pirih <david.pirih@gmx.de>
2015-05-19 01:15:30 +0200

    start (fade in), win and lose scene has been set up, persisting user options are implemented and level scene prepared (grass tiles)

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+	
+	private AudioSource audioSource;
+	
+	void Awake () {
+		DontDestroyOnLoad (gameObject);
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	public void SetVolume (float volume) {
+		audioSource.volume = volume;	
+	}
+	
+	void OnLevelWasLoaded () {
+		AudioClip currentLevelMusicClip = levelMusicChangeArray[Application.loadedLevel];
+		if (currentLevelMusicClip) {
+			audioSource.clip = currentLevelMusicClip;
+			audioSource.loop = true;
+			audioSource.Play ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPrefsManager : MonoBehaviour {
+
+	const string MASTER_VOLUME_KEY = "master_volume";
+	const string DIFFICULTY_KEY = "difficulty";
+	const string LEVEL_KEY = "level_unlocked_";
+
+	public static void SetMasterVolume (float volume) {
+		if (volume >= 0f && volume <= 1f) {
+			PlayerPrefs.SetFloat (MASTER_VOLUME_KEY, volume);
+		} else {
+			Debug.LogError ("Master volume out of range! Value: " + volume); 
+		}
+	}
+	
+	public static float GetMasterVolume () {
+		return PlayerPrefs.GetFloat (MASTER_VOLUME_KEY);
+	}
+	
+	public static void UnlockLevel (int level) {
+		if (level < Application.levelCount - 1) {
+			PlayerPrefs.SetInt (LEVEL_KEY + level.ToString(), 1); // use 1 as true  
+		} else {
+			Debug.LogError ("Level to unlock is out of range! Level: " + level); 
+		}
+	} 
+	
+	public static bool IsLevelUnlocked (int level) {
+		bool isLevelUnlocked = PlayerPrefs.GetInt (LEVEL_KEY + level.ToString()) == 1;
+		if (level < Application.levelCount - 1) {
+			return isLevelUnlocked;
+		} else {
+			Debug.LogError ("Requested Level is out of range! Level: " + level);
+			return false; 
+		}
+	}
+	
+	public static void SetDifficulty (float value) {
+		if (value >= 1f && value <= 3f) {
+			PlayerPrefs.SetFloat (DIFFICULTY_KEY, value);
+		} else {
+			Debug.LogError ("Difficulty out of range! Value: " + value); 
+		}	
+	}
+	
+	public static float GetDifficulty () {
+		return PlayerPrefs.GetFloat (DIFFICULTY_KEY);
+	}
+}
