ad2b35635029b3c664fba51ccea8f037f91d7bad
David Pirih <david.pirih@gmx.de>
2015-05-27 22:43:05 +0200

    refactoring: defender / projectiles parent is now code generated // defender spawn implemented

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefenderSpawner : MonoBehaviour {
+
+	public Camera myCamera;
+
+	private GameObject defenderParent;
+	
+	void Start () {
+		defenderParent = GameObject.Find ("Defenders");
+		if (!defenderParent) {
+			defenderParent = new GameObject("Defenders");
+		}
+	}
+
+	void OnMouseDown () {
+		Vector2 targetPos = SnapToGrid (CalculateWorldPointOfMousePosition ());
+		if (Button.selectedDefender) {
+			GameObject defender = Instantiate(Button.selectedDefender, targetPos, Quaternion.identity) as GameObject;
+			defender.transform.parent = defenderParent.transform;
+		}
+
+	}
+
+	Vector2 CalculateWorldPointOfMousePosition () {
+		// distance is uninportant in 2D Games with orthograhic camera
+		float distanceFromCamera = 10f;
+
+		Vector3 weirdTriplet = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distanceFromCamera);
+		Vector2 worldPos = myCamera.ScreenToWorldPoint (weirdTriplet);
+
+		return worldPos;
+	}
+
+	Vector2 SnapToGrid (Vector2 rawWorldPos) {
+		return new Vector2(Mathf.Round(rawWorldPos.x), Mathf.Round(rawWorldPos.y));
+	}
+}
